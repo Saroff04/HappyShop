d024c12a348129ce3861105d326624aa2e21dc55
saroff <saroff04@gmail.com>
2025-12-28 17:16:56 +0000

    1.1

--- src/main/java/ci553/happyshop/client/customer/CustomerClient.java
+++ src/main/java/ci553/happyshop/client/customer/CustomerClient.java
@@ -17,7 +17,7 @@ public class CustomerClient extends Application {
     }
 
     /**
-     * Creates the Model, View, and Controller objects and links them together for communication.
+     *11 Creates the Model, View, and Controller objects and links them together for communication.
      * It also creates the DatabaseRW instance via the DatabaseRWFactory and injects it into the CustomerModel.
      * Once the components are linked, the customer interface (view) is started.
      *
