6891d0de2bcc6afb7d155aed4e4374a6a40c2caf
saroff <saroff04@gmail.com>
2026-01-07 07:21:05 +0000

    Removed incode sound, added new SoundFX to get called for sound instead

--- src/main/java/ci553/happyshop/utility/SoundFX.java
+++ src/main/java/ci553/happyshop/utility/SoundFX.java
@@ -2,12 +2,12 @@ package ci553.happyshop.utility;
 
 import  javafx.scene.media.AudioClip;
 
-public final class SoundFX {
+public final class SoundFX { // adding the sound
     private SoundFX(){}
         private static final AudioClip clickSound =
                 new AudioClip(SoundFX.class.getResource("/click.mp3").toString());
 
-        public static void click() {
+        public static void click() { // class to be called and play the click sound
             clickSound.play();
         }
 }
